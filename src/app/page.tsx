bc86936f7bb93a28a98611c0ad1b2c59699b5916
hamatai7109 <taishihamano0515@gmail.com>
2023-09-29 15:26:52 +0900

    add side-bar and modified some layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-import { PostType } from "./types";
-import Header from "@/components/base/header/header";
-
-// 全記事取得API
-async function fetchAllBlog() {
-  const res = await fetch(`http://localhost:3000/api/blog`, {
-    cache: "no-store", //SSR(Server Side Rendering) → 更新が頻繁に行われるブログなどに有効。
-  });
-
-  const data = await res.json();
-
-  return data.posts;
-}
+import ListAllPosts from "@/components/section/listAllPosts";
+import SideBar from "@/components/section/sideBar";
 
 export default async function Home() {
-  const posts = await fetchAllBlog();
 
   return (
     <main className="w-full h-full">
@@ -24,38 +10,9 @@ export default async function Home() {
           Next js 練習用のブログ
         </h1>
       </div>
-      <div className="w-full flex flex-col justify-center items-center">
-        {posts.map(
-          (
-            post: PostType //map関数で、配列（ブログ記事）を順番に並べていく。()だとreturn省略OK、{}だとreturnが必要
-          ) => (
-            <div
-              key={post.id}
-              className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center"
-            >
-              <div className="flex items-center my-3">
-                <div className="mr-auto">
-                  <h2 className="mr-auto font-semibold">{post.title}</h2>
-                </div>
-                <Link
-                  href={`/blog/edit/${post.id}`}
-                  className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
-                >
-                  編集
-                </Link>
-              </div>
-              <div className="mr-auto my-1">
-                <blockquote className="font-bold text-slate-700">
-                  {new Date(post.date).toDateString()}
-                </blockquote>
-              </div>
-
-              <div className="mr-auto my-1">
-                <h2>{post.description}</h2>
-              </div>
-            </div>
-          )
-        )}
+      <div className="flex">
+        <ListAllPosts/>
+        <SideBar />
       </div>
     </main>
   );
---
+++ src/components/section/sideBar.tsx
@@ -0,0 +1,15 @@
+export default function SideBar(){
+  return (
+    <div className="w-1/3">
+      <div className="w-3/4 h-10 bg-slate-300 mt-6 mx-auto">
+        プロフィール
+      </div>
+      <div className="w-3/4 h-10 bg-slate-300 mt-6 mx-auto">
+        最近の記事
+      </div>
+      <div className="w-3/4 h-10 bg-slate-300 mt-6 mx-auto">
+        カテゴリー or タグ
+      </div>
+    </div>  
+  )
+}
