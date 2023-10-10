4c934b76e11e77e17c20dff4920942bd1de334bf
hamatai7109 <taishihamano0515@gmail.com>
2023-10-10 10:03:18 +0900

    setting prettier

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const DashBoard = () => {
   return (
     <>
-    <div>
-      <Link href={"/admin/add"}>記事を作成する</Link>
-      <Link href={"/admin/edit"}>記事を編集する</Link>
-      <Link href={"/admin/profile"}>プロフィールを編集する</Link>
+      <div>
+        <Link href={"/admin/add"}>記事を作成する</Link>
+        <Link href={"/admin/edit"}>記事を編集する</Link>
+        <Link href={"/admin/profile"}>プロフィールを編集する</Link>
       </div>
-      <div>Log out</div></>
-  )
-}
+      <div>Log out</div>
+    </>
+  );
+};
 
-export default DashBoard
+export default DashBoard;
